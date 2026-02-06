17fae56b9739ad9869b81545e88b7e087079ca80
bhakuniajay <120650107+bhakuniajay@users.noreply.github.com>
2026-02-06 05:21:07 -0800

    Update ec2.tf

--- compute/ec2.tf
+++ compute/ec2.tf
@@ -1,1 +1,31 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  key_name               = "free-tier-key"   # create once in AWS console
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "<h1>Terraform EC2 - Free Tier Project</h1>" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "free-tier-ec2"
+  }
+}
 
