2cb9c14380d92ff21cc7fd624100aeda84d73184
bhakuniajay <120650107+bhakuniajay@users.noreply.github.com>
2026-02-06 05:32:57 -0800

    Update ci_role.tf

--- iam/ci_role.tf
+++ iam/ci_role.tf
@@ -1,1 +1,34 @@
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
+}
+resource "aws_iam_policy" "terraform_ci_policy" {
+  name = "terraform-ci-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:*",
+          "elasticloadbalancing:*",
+          "autoscaling:*",
+          "iam:GetRole",
+          "iam:PassRole",
+          "s3:*",
+          "dynamodb:*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
 
