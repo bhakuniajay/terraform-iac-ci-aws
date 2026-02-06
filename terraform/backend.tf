fb627432b31b5e3ae23622836ce2aec5d3cd1e2f
bhakuniajay <120650107+bhakuniajay@users.noreply.github.com>
2026-02-06 05:09:55 -0800

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,1 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-ajay"
+    key            = "iac-ci/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
 
