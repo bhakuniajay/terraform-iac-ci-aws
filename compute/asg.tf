e4b359308e1b3534db5ff3327c6703a0a5358702
bhakuniajay <120650107+bhakuniajay@users.noreply.github.com>
2026-02-06 05:24:58 -0800

    Update asg.tf

--- compute/asg.tf
+++ compute/asg.tf
@@ -1,1 +1,27 @@
+resource "aws_launch_template" "web_lt" {
+  name_prefix   = "free-tier-lt-"
+  image_id      = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+  key_name      = "free-tier-key"
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = base64encode(<<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "<h1>ASG EC2 - Terraform Free Tier</h1>" > /var/www/html/index.html
+              EOF
+  )
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "asg-free-tier-ec2"
+    }
+  }
+}
 
