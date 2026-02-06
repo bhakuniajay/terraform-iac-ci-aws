c3e2ddb5d22d87227199495bcc2371fc67e02e08
bhakuniajay <120650107+bhakuniajay@users.noreply.github.com>
2026-02-06 05:22:32 -0800

    Update alb.tf

--- loadbalancer/alb.tf
+++ loadbalancer/alb.tf
@@ -1,1 +1,44 @@
+resource "aws_lb" "web_alb" {
+  name               = "free-tier-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.web_sg.id]
+  subnets            = [aws_subnet.public.id]
+
+  tags = {
+    Name = "free-tier-alb"
+  }
+}
+
+resource "aws_lb_target_group" "web_tg" {
+  name     = "free-tier-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_target_group_attachment" "web_attach" {
+  target_group_arn = aws_lb_target_group.web_tg.arn
+  target_id        = aws_instance.web.id
+  port             = 80
+}
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.web_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_tg.arn
+  }
+}
 
