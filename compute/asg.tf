64adf1cf1449563abd77b0384fe6a2ddc760cb4c
bhakuniajay <120650107+bhakuniajay@users.noreply.github.com>
2026-02-06 05:25:54 -0800

    Update asg.tf

--- compute/asg.tf
+++ compute/asg.tf
@@ -24,4 +24,27 @@ resource "aws_launch_template" "web_lt" {
     }
   }
 }
+resource "aws_autoscaling_group" "web_asg" {
+  desired_capacity = 1
+  max_size         = 1
+  min_size         = 1
+
+  vpc_zone_identifier = [aws_subnet.public.id]
+
+  launch_template {
+    id      = aws_launch_template.web_lt.id
+    version = "$Latest"
+  }
+
+  target_group_arns = [aws_lb_target_group.web_tg.arn]
+
+  health_check_type         = "ELB"
+  health_check_grace_period = 300
+
+  tag {
+    key                 = "Name"
+    value               = "free-tier-asg"
+    propagate_at_launch = true
+  }
+}
 
